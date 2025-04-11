45eaa42705521b88ed05e41a85765ae1a462d4c8
fpoggi <francesco.poggi5@unibo.it>
2025-04-11 16:28:27 +0200

    initial commit

---
+++ it.knowledgex.seminar.jena/src/main/java/it/knowledgex/seminar/jena/App2.java
@@ -0,0 +1,63 @@
+package it.knowledgex.seminar.jena;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFWriter;
+import org.apache.jena.vocabulary.VCARD;
+
+/**
+ * Hello world!
+ */
+public class App2 {
+	
+	public static void main(String[] args) {
+		// some definitions
+		String personURI    = "http://somewhere/JohnSmith";
+		String fullName     = "John Smith";
+
+		// create an empty Model
+		Model model = ModelFactory.createDefaultModel();
+		
+		// create the resource
+		Resource johnSmith = model.createResource(personURI);
+
+		// add the property
+		johnSmith.addProperty(VCARD.FN, fullName);
+
+		Resource francescoPoggi = createPerson(model, "Michele", "Pellegrini");
+		Resource andreaNuzzolese = createPerson(model, "Jerry", "Calà");
+		
+		// now write the model in XML form to a file
+		model.write(System.out, "TURTLE");
+		
+		RDFWriter.source(model)
+	     //.set(RIOT.symTurtleIndentStyle, "long")
+	     .lang(Lang.RDFXML)
+	     .output(System.out);
+	}
+
+	public static Resource createPerson(Model model, String gn, String fn) {
+		// some definitions
+		String personURI    = "http://somewhere/" + gn.replace(" ", "_") + fn;
+		String givenName    = gn;
+		String familyName   = fn;
+		String fullName     = givenName + " " + familyName;
+
+		// create an empty Model
+		//Model model = ModelFactory.createDefaultModel();
+
+		// create the resource
+		//   and add the properties cascading style
+		Resource person
+		  = model.createResource(personURI)
+		         .addProperty(VCARD.FN, fullName)
+		         .addProperty(VCARD.N,
+		                      model.createResource()
+		                           .addProperty(VCARD.Given, givenName)
+		                           .addProperty(VCARD.Family, familyName));
+		return person;
+		
+	}
+}
